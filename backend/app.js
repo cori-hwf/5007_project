a1ea7a2d38ebb5b806f0d626bc3876848fbdf4c4
anna <fangziyingice@gmail.com>
2022-04-23 12:28:26 +0800

    backend addded, login&createAcc tested

---
+++ backend/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql').graphqlHTTP; //a middleware that takes in incoming request -> funnel through graphql parser -> forward to resolvers
+const mongoose = require('mongoose');
+
+const graphQlSchema = require('./graphql/schema/index');
+const resolvers = require('./graphql/resolvers/index')
+//const {buildSchema} = require('graphql');
+//const bcrypt = require('bcryptjs'); //to hash password for security sake
+
+//const User = require('./models/user');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req,res,next) => { //resolve the CORS issue
+    res.setHeader('Access-Control-Allow-Origin','*') //any clients are allowed to send request to this server
+    res.setHeader('Access-Control-Allow-Methods','POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization'); //Headers that can be sent to the server 
+    if (req.method === 'OPTIONS'){return res.sendStatus(200);}
+    next(); //if it's not options -> continue to next
+})
+
+app.use('/graphql',
+     graphqlHttp({
+    schema:graphQlSchema,
+    rootValue:resolvers, //resolver
+
+    graphiql: true
+}));
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.bebr0.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
+.then(() => {
+    app.listen(8081);
+    console.log("connected to db")
+}).catch(err => {console.log(err)});
+
--- frontend/src/CreateAccount.js
+++ frontend/src/CreateAccount.js
@@ -1,8 +1,55 @@
 import React from "react";
 
+
+async function graphQLFetch(query, variables = {}) {
+    try {
+      const response = await fetch('http://localhost:8081/graphql', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({ query, variables })
+      });
+      const body = await response.text();
+      const result = JSON.parse(body);
+      //console.log(`body is ${body}`)
+
+      if (result.errors) {
+        const error  = result.errors[0]
+        alert(`${error.message}`);}
+      
+      return result.data.createUser ? result.data : 'existing account';
+    } catch (e) {
+      if (!e.message){alert("You have an existing account, please log in.")}
+      else alert(`Error in sending data to server: ${e.message}`);
+    }
+  }
+
 class CreateAccount extends React.Component{
     constructor() {
         super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    async handleSubmit(e){
+        e.preventDefault();
+        const form = document.forms.createAcc;
+        const email = form.username.value;
+        const password = form.password.value;
+        if (email.trim().length === 0 || password.trim().length === 0){
+            alert("Email address and Password fields are compulsary.")
+            return;
+        }
+        const query = `mutation {
+        createUser(userInput: {email: "${email}", password: "${password}"})  {
+        _id
+        email
+        }
+        }`;    
+        
+        const data = await graphQLFetch(query);
+        console.log(data)
+        form.username.value = ""; form.password.value = "";
+        if (data!="existing account"){alert("Account created");}
+
     }
 
     render() {
@@ -10,10 +57,10 @@ class CreateAccount extends React.Component{
             <div className="login-root">
                 <div className="formbg">
                     <span className="signInTitle">Create your account</span>
-                    <form id="stripe-login">
+                    <form name="createAcc" id="stripe-login" onSubmit={this.handleSubmit}>
                         <div className="field">
                             <label htmlFor="email">Name</label>
-                            <input type="text" name="username" placeholder="Your User Name"/>
+                            <input type="text" name="username" placeholder="Your Email Address"/>
                         </div>
                         <div className="field">
                             <label htmlFor="password">Password</label>
